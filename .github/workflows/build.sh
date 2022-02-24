f631ea6c355c397b2cd3ed85922ad0c259424f25
Thomas Aarholt <thomasaarholt@gmail.com>
2022-02-24 10:55:54 +0100

    ls

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -23,6 +23,7 @@ echo "::group::Install a virtualenv"
 echo "::endgroup::"
 
 echo "::group::Build wheel"
+  ls -la
   clean_code $REPO_DIR $BUILD_COMMIT
   build_wheel $REPO_DIR $PLAT
   ls -l "${GITHUB_WORKSPACE}/${WHEEL_SDIR}/"
